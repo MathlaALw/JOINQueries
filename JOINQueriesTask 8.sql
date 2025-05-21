e557987837090cb9d91fbf8f7a04547de47a823b
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-05-21 09:34:18 +0400

    Adding  Airline Database – JOIN Queries SQL File

--- JOINQueriesTask 8.sql
+++ JOINQueriesTask 8.sql
@@ -131,9 +131,109 @@ select S.Fname +' ' +S.Lname as 'Student Name' , C.Course_Name as 'Course Name '
 -- 10.  For each class in Building 'Main', retrieve class ID, course name, department name, and faculty name teaching the class. 
 
 
+select * from Student;
+select * from Course;
+select * from Faculty;
+select S.Fname +' ' +S.Lname as 'Student Name' , C.Course_Name as 'Course Name ', D.D_Name as 'Department Name ' from Student S 
+inner join Course C  ON C.SID = S.SID
+inner join Faculty F  ON F.FID = S.FID
+inner join Department D on D.Department_ID =F.Department_ID
+
+
 
 -- 11.  Display all faculty members who manage any department. 
 
+select * from Faculty;
+select * from Department;
+
+SELECT F.* FROM Faculty F , Department D WHERE D.Department_ID = F.Department_ID ;
 
 -- 12.  Display all students and their advisors' names, even if some students don’t have advisors yet.
 
+select * from Student;
+select * from Faculty;
+
+SELECT S.* , F.F_Name as 'advisors name' FROM Student S LEFT OUTER JOIN Faculty F ON F.FID = S.FID;
+
+-------------------------------------------
+
+-- Airline Database – JOIN Queries
+use airline;
+
+-- 1. Display each flight leg's ID, schedule, and the name of the airplane assigned to it. 
+
+select * from FlightLeg;
+select * from Airport;
+
+select F.Leg_No ,  F.Sechuled_Dep_Time ,  F.Sechuled_Arr_Time, A.A_Name from FlightLeg F inner join Airport A on A.Airport_Code=F.Airport_Code
+-- 2. Display all flight numbers and the names of the departure and arrival airports. 
+select * from FlightLeg;
+select * from Airport;
+select * from Flight
+select * from LegInstance;
+
+select F.Number as 'flight numbers' , A.A_Name as  'departure_airport' , A.A_Name as 'arrival_airport' from Flight F 
+inner join  FlightLeg FL on F.Number = FL.Number
+inner join Airport A on A.Airport_Code = F.Number
+
+
+
+-- 3. Display all reservation data with the name and phone of the customer who made each booking. 
+select * from Reservation;
+
+select R_Date as 'Reservation Data', Customer_Name as 'Customer Name' , CPhone as 'Customer Phone' from Reservation;
+
+-- 4. Display IDs and locations of flights departing from 'CAI' or 'DXB'. 
+select * from Flight
+-- select Number as ' Flight ID ', Location  from Flight where Departing from 'CAI' or 'DXB'
+
+-- 5. Display full data of flights whose names start with 'A'. 
+
+select * from Flight where Airline like 'A%'
+
+-- 6. List customers who have bookings with total payment between 3000 and 5000. 
+
+-- select Customer_Name as 'Customer Name'  from Reservation where Total between 3000 and 5000;
+
+-- 7. Retrieve all passengers on 'Flight 110' who booked more than 2 seats. 
+
+select * from Flight
+select * from Reservation
+select * from LegInstance
+select * from Seats
+
+
+select R.Customer_Name as 'Customer Name'  from Reservation R inner join Seats S on S.Seat_No = R.Seat_No
+inner join LegInstance L on L.Leg_Date = R.Leg_Date
+inner join FlightLeg FL on FL.Leg_No = L.Leg_No
+inner join Flight F on F.Number = FL.Number 
+where S.Seat_No > 2 ;
+
+-- 8. Find names of passengers whose booking was handled by agent "Youssef Hamed". 
+
+--  select Customer_Name as 'Customer Name'  from Reservation 
+
+-- 9. Display each passenger’s name and the flights they booked, ordered by flight date. 
+
+select R.Customer_Name as 'Customer Name' , F.Airline from Reservation R inner join LegInstance L on L.Leg_Date = R.Leg_Date
+inner join FlightLeg FL on Fl.Leg_No =L.Leg_No
+inner join Flight F on F.Number=Fl.Number
+order by L.Leg_Date
+
+-- 10.  For each flight departing from 'Cairo', display the flight number, departure time, and airline name. 
+
+SELECT F.Number,FL.Sechuled_Dep_Time,A.A_Name FROM Flight F
+inner join FlightLeg FL ON F.Number = F.Number
+inner join Airport A ON FL.Airport_Code = A.Airport_Code
+WHERE A.A_Name = 'Cairo';
+
+-- 11.  Display all staff members who are assigned as supervisors for flights. 
+
+--SELECT s.staff_id,s.staff_name,s.role FROM Staff s inner join Flights f ON s.staff_id = f.supervisor_id;
+
+-- 12.  Display all bookings and their related passengers, even if some bookings are unpaid.
+
+-- SELECT b.booking_id,b.paid,p.passenger_id,p.passenger_name FROM Bookings b LEFT JOIN Passengers p ON b.passenger_id = p.passenger_id;
+
+
+------------------------------------------
